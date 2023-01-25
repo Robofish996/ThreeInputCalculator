2edb88d5cf1ce1f9994088775fbd93d1215bb61c
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-01-25 10:30:57 +0200

    Figuring out how to add values

---
+++ functions/if-cal.js
@@ -0,0 +1,13 @@
+function myFunction() {
+
+    //defining number varaiables using let as numbers can change
+    let htmlNumOne = document.getElementById('numInputOne').value;
+    let htmlNumTwo = document.getElementById('numInputTwo').value;
+    let htmlAction = document.getElementById('action');
+
+   
+
+
+   
+  
+}
