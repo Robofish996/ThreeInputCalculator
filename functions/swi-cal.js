2daae2656f7437458e92e50ebb7717b772464f60
Robofish996 <114180692+Robofish996@users.noreply.github.com>
2023-01-26 20:00:24 +0000

    Completed ThreeInput Calculator

--- functions/swi-cal.js
+++ functions/swi-cal.js
@@ -1,19 +1,34 @@
-function myFunction(){
-// Setting variables of number A and B
-let numInputOne = parseFloat(document.getElementById('inputOne').value);
-let numInputTwo =parseFloat(document.getElementById('inputTwo').value);
+function myFunction() {
+  // Setting variables of number A and B
+  let numInputOne = parseFloat(document.getElementById('inputOne').value);
+  let numInputTwo = parseFloat(document.getElementById('inputTwo').value);
 
-// Setting variable for plus,minus,devide and times. + - * / 
-let htmlAction = document.getElementById('action').value;
+  // Setting variable for plus,minus,devide and times. + - * / 
+  let htmlAction = document.getElementById('action').value;
 
-//using switch statement to preform calculation
-let swiAnswer;
-switch(htmlAction) {
+  //using switch statement to preform calculation
+  let swiAnswer;
+  switch (htmlAction) {
     case "+":
-        swiAnswer = numInputOne + numInputTwo
+      swiAnswer = numInputOne * numInputTwo
       break;
+    case "-":
+      swiAnswer = numInputOne * numInputTwo
+      break;
+    case "/":
+      swiAnswer = numInputOne * numInputTwo
+      break;
+    case "*":
+      swiAnswer = numInputOne * numInputTwo
+      break;
+    default:
+      swiAnswer = "Please enter the required information"
 
 
-console.log(swiAnswer);
-}
+
+  }
+
+  // display the result
+  document.getElementById('result').innerHTML += `${numInputOne}  ${htmlAction} ${numInputTwo} = ${swiAnswer}`;
+
 }
